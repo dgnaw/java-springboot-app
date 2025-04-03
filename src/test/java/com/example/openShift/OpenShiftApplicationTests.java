a0a24b9f074f4d0d2b26d8a5e674552d15d19bee
dgnaw <dogianam09072005@gmail.com>
2025-04-03 19:58:39 +0700

    Init commit

---
+++ src/test/java/com/example/openShift/OpenShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openShift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenShiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
